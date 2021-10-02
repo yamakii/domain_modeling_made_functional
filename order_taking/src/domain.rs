664e9cfc90c9b7e4f641516d3c2bee18165a127c
yamakii <hiroshi_yamaki@caddi.jp>
2021-10-02 23:24:56 +0900

    add domain

--- order_taking/src/domain.rs
+++ order_taking/src/domain.rs
@@ -1,7 +1,20 @@
 use anyhow::Result;
 
-struct OrderId(String);
+struct WidgetCode(String);
+struct GizmoCode(String);
+enum ProductCode {
+    Widget(WidgetCode),
+    Gizmo(GizmoCode),
+}
 
+struct UnitQuantity(i32);
+struct KilogramQuantity(f64);
+enum OrderQuantity {
+    Unit(UnitQuantity),
+    Kilogram(KilogramQuantity),
+}
+
+struct OrderId(String);
 impl OrderId {
     pub fn new(value: String) -> Result<Self> {
         if value.is_empty() {
@@ -16,4 +29,57 @@ impl OrderId {
     pub fn value(&self) -> &str {
         &self.0
     }
+}
+
+struct OrderLineId();
+struct CustomerId();
+
+struct CustomerInfor();
+struct ShippingAddress();
+struct BillingAddress();
+struct Price();
+struct BillingAmount();
+
+struct Order {
+    id: OrderId,
+    customer_id: CustomerId,
+    shipping_address: ShippingAddress,
+    billing_address: BillingAddress,
+    order_lines: (),
+    amount_to_bill: BillingAmount,
+}
+
+struct OrderLine {
+    id: OrderLineId,
+    oder_id: OrderId,
+    product_code: ProductCode,
+    order_quantity: OrderQuantity,
+    price: Price,
+}
+
+// to be
+
+fn place_order(order: UnvalidatedOrder) -> Result<PlaceOrderEvent, PlaceOrderEror> {
+    unimplemented!()
+}
+
+struct UnvalidatedOrder {
+    order_id: String,
+    customer_info: (),
+    shipping_address: (),
+}
+
+struct PlaceOrderEvent {
+    acknowledgement_sent: (),
+    order_placed: (),
+    billable_order_placed: (),
+}
+
+enum PlaceOrderEror {
+    ValidationError(Vec<ValidationError>),
+}
+
+struct ValidationError {
+    fieled_name: String,
+    error_description: String,
 }
